04fff3c516c9213a1b420015437fd9bc260228ea
Count <countjr+cloud9@gmail.com>
2017-04-08 20:33:06 +0000

    fix makefile

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 start:
-	DEBUG=*,-babel npm run nodemon -- --watch src --ext '.js,.pug' --exec babel-node -- src/server.js
+	DEBUG=*,-babel,-nodemon npm run nodemon -- --watch src --ext '.js,.pug' --exec babel-node -- src/server.js
--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import http from 'http';
+import url from 'url';
 // import User from './User';
 
 const server = new http.Server((req, res) => {
-    console.log(req.method, req.url);
-    res.end('hello, vasja');
+    console.log(/*req.method, req.url, url.parse(req.url, true)*/ req.headers);
+    const urlParsed = url.parse(req.url, true);
+    if (urlParsed.pathname === '/echo' && urlParsed.query.message) {
+        res.end(urlParsed.query.message);
+    } else {
+        res.statusCode = 404;
+        res.end('page not found');
+    }
+    
 });
 // const vasja = new User('vasja');
 
